2345fbac3361506e0c318f06fc45a22c7431ab3c
AnassEREKYSY <ereanass@gmail.com>
2025-09-02 15:44:14 +0200

    Fixed the nav bar and sidebar style

--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
-// material
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
